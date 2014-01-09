20729cc2f494831908df8de8c502459144e3b3dd
Matthew Congrove <mattcongrove@gmail.com>
2014-01-09 05:18:57 -0500

    Improving localStorage

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -8,8 +8,8 @@ Pebble.addEventListener("showConfiguration", function(e) {
 	var url = "http://wearewearable.com/daylight/?v=1.0";
 	
 	if(options != null) {
-		url += "&theme=" + encodeURIComponent(options["0"]) +
-			"&offset=" + encodeURIComponent(options["1"][0]);
+		url += "&theme=" + (options["0"] ? encodeURIComponent(options["0"]) : "");
+			"&offset=" + (options["1"] && options["1"][0] != null ? encodeURIComponent(options["1"][0]) : "");
 	}
 	
 	Pebble.openURL(url);
